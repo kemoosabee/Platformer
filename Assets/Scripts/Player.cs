40586d7e009543191b6d0719a844ac4425d05dca
conor walters <waltersn63@gmail.com>
2020-05-14 23:43:51 +0100

    Added Jumping, animations and moving

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,25 +1,72 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     [SerializeField]
-    private float _speed = 6.0f;
-    [SerializeField]
-    private float _jumpSpeed = 6.0f;
-    [SerializeField]
+    private float _speed = 2.0f;
     private float _moveInput;
     private Rigidbody2D _rigidbody;
+    private bool _facingRight = false;
+
+
+    [SerializeField]
+    private float _jumpSpeed = 2.0f;
+    private int _extraJumps;
+    private int _extraJumpValue = 1;
+    [SerializeField]
+    private Transform _groundCheck;
+    private bool _isGrounded;
+    [SerializeField]
+    private float _checkRadius;
+    [SerializeField]
+    private LayerMask _whatIsGround;
+
     void Start()
     {
+        _extraJumps = _extraJumpValue;
         _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if(_isGrounded)
+        {
+            _extraJumps = _extraJumpValue;
+        }
+        if(Input.GetKeyDown(KeyCode.UpArrow) && _extraJumps > 0)
+        {
+            _rigidbody.velocity = Vector2.up * _jumpSpeed;
+            _extraJumps --;
+        }else if(Input.GetKeyDown(KeyCode.UpArrow) && _extraJumps == 0 && _isGrounded == true)
+        {
+             _rigidbody.velocity = Vector2.up * _jumpSpeed;
+        }
     }
 
     void FixedUpdate()
     {
-        _moveInput = Input.GetAxis("Horizontal");
+        ControlPlayerMovement();
+    }
+
+     void ControlPlayerMovement()
+    {
+        _isGrounded = Physics2D.OverlapCircle(_groundCheck.position, _checkRadius,_whatIsGround);
+        _moveInput = Input.GetAxisRaw("Horizontal");
         _rigidbody.velocity = new Vector2(_moveInput * _speed, _rigidbody.velocity.y);
+
+        if(_facingRight == false && _moveInput > 0)
+        {
+            _facingRight = true;
+             transform.eulerAngles = new Vector3(0, -180, 0);
+        }
+        else if(_facingRight == true && _moveInput < 0)
+        {
+            _facingRight = false;
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
     }
 }
 
