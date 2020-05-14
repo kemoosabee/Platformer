aeeb72573500696d134f13951c179a5600da4a0f
conor walters <waltersn63@gmail.com>
2020-05-14 18:41:50 +0100

    Changed project Settings

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private CharacterController _CharacterController;
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("HI");
+         _CharacterController = GetComponent<CharacterController>();
+
+         if(_CharacterController == null)
+         {
+             Debug.Log("Character Controller is null");
+         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
